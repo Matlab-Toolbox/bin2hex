7ed13313158491ac620046905d6c0687af034f21
Morgan Prior <morgan@amaras-tech.co.uk>
2014-03-25 19:12:56 +0000

    Hit 52 bit limit (hardwired matlab limit) to early, due num2str inserting extra spaces in string when tested with Arrays.

--- function/bin2hex.m
+++ function/bin2hex.m
@@ -11,8 +11,10 @@ function [ output_string ] = bin2hex( bin_string )
 input_is_char          = ischar(bin_string);
 %% Convert to string if input not a string
 %    This can happen for conversions of integers made up of 1 and 0s
+%    Remove all spaces, Vector conversion puts '  ' between bits
 if ~input_is_char
     bin_string = num2str( bin_string );
+    bin_string = strrep(bin_string, ' ', '');    
 end
 
 output_string = dec2hex( bin2dec( bin_string )) ;
