4f6dfc1a6bd3ff549efae98f3b60fb79899f0513
Morgan Prior <morgan@amaras-tech.co.uk>
2014-03-25 19:02:35 +0000

    Added bin2hex test

---
+++ test/run_bin2hex_test.m
@@ -0,0 +1,53 @@
+
+  %% run_spec
+  clear;
+  %% Clear import list to test correct library
+  clear import; 
+
+  %% For Functions
+  %    Add function to path
+  addpath('../function/');
+
+  %% Test vectors
+  test ={};
+  %% String input
+  test(end+1).input =        '0';   test(end).expect =  '0' ;
+  test(end+1).input =        '1';   test(end).expect =  '1' ;
+  test(end+1).input =       '10';   test(end).expect =  '2' ;
+  test(end+1).input =       '11';   test(end).expect =  '3' ;
+  test(end+1).input =      '100';   test(end).expect =  '4' ;
+  test(end+1).input =      '101';   test(end).expect =  '5' ;
+  test(end+1).input =      '110';   test(end).expect =  '6' ;
+  test(end+1).input =      '111';   test(end).expect =  '7' ;
+  test(end+1).input =     '1000';   test(end).expect =  '8' ;
+  test(end+1).input =     '1001';   test(end).expect =  '9' ;
+  test(end+1).input =     '1010';   test(end).expect =  'A' ;
+  test(end+1).input =     '1011';   test(end).expect =  'B' ;
+  test(end+1).input =     '1100';   test(end).expect =  'C' ;
+  test(end+1).input =     '1101';   test(end).expect =  'D' ;
+  test(end+1).input =     '1110';   test(end).expect =  'E' ;
+  test(end+1).input =     '1111';   test(end).expect =  'F' ;
+ 
+  %% The tests
+  error_count = 0;
+  pass_count  = 0;
+  
+  for vector=1:size(test,2)
+     b       = bin2hex(test(vector).input);
+     if (  ~isequal(b, test(vector).expect)  )
+       disp('bin2hex() Failed ')
+       error_count = error_count + 1;
+     else
+       pass_count  = pass_count  + 1;
+     end
+  end
+  
+   
+  
+  %% Test Status Report
+  if error_count > 0
+    disp(['Test FAILED : ', num2str(pass_count), ' passes and ',num2str(error_count),' fails'])
+  else
+    disp(['Test Passed : ', num2str(pass_count), ' checks ran '])
+  end
+
